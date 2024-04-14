621f960dd685a7de86f9f93f2e5085003887d142
Study Buffalo <joshua@torrance.io>
2024-04-13 20:28:54 -0600

    Reworked to support prompting and saving functions

--- extract/__main__.py
+++ extract/__main__.py
@@ -1,10 +1,26 @@
 """Extracts details from AHS paycheque PDF."""
-from pprint import pprint
+from utils import (
+    extract_data, generate_config, identify_pdf, setup_logging, save_data,
+)
 
-from utils import extract_data, generate_config
 
+def main():
+    """Main function to run application."""
+    # Setup Config and Logging details
+    config = generate_config()
+    log = setup_logging(config)
 
-CONFIG = generate_config()
-DATA = extract_data(CONFIG)
+    # Identify which PDF to extract data from
+    pdf_path = identify_pdf(config, log)
 
-pprint(DATA)
+    if pdf_path is None:
+        return
+
+    # Extract Data from selected PDF
+    data = extract_data(pdf_path, config, log)
+
+    # Save PDF data to Excel file
+    save_data(data.data, config, log)
+
+if __name__ == '__main__':
+    main()
